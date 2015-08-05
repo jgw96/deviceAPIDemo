52e89bd60690ad6bde5017f37c71d782c8465759
Justin Willis <jgw9617@gmail.com>
2015-08-05 01:01:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,5 @@ window.addEventListener('devicelight', function(e) {
 var battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery;
 document.querySelector("#battLevel").textContent = battery.level;
 
-navigator.getBattery().then(function(result){
-  document.querySelector("#battLevel").textContent = result.level;
-})
 
 
