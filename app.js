6042d5443438ef5d06b2f8edca1b7e9c1b4b1542
Justin Willis <jgw9617@gmail.com>
2015-08-05 01:42:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,28 @@ document.querySelector("#vibrateOnceButton").addEventListener("click",function()
 
 document.querySelector("#vibrateMultipleButton").addEventListener("click",function(){
   navigator.vibrate([3000, 2000, 1000]);
-})
+});
+
+navigator.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
+
+if (navigator.getUserMedia) {
+   navigator.getUserMedia({ video: { width: 1280, height: 720 } },
+      function(stream) {
+         var video = document.querySelector('video');
+         video.src = window.URL.createObjectURL(stream);
+         video.onloadedmetadata = function(e) {
+           video.play();
+         };
+      },
+      function(err) {
+         console.log("The following error occured: " + err.name);
+      }
+   );
+} else {
+   console.log("getUserMedia not supported");
+}
 
 
 
