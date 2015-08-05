780b5c570d32804827f05919e492312a86808fe8
Justin Willis <jgw9617@gmail.com>
2015-08-05 00:38:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 
-/* Ambient Light Events */
-window.addEventListener('devicelight', function(deviceLightEvent) {
 
-    /* Check ambient light status */
-    if (deviceLightEvent.value < 500) { // snow
-        document.querySelector("#wow").text="It is dim";
-    }
-   
+window.addEventListener('devicelight', function(e) {
+  var lux = e.value;
+  document.querySelector('#wow').textContent = lux + ' lux';
+  
+  if(lux < 50) {
+    document.body.className = 'dim';
+  }
+  if(lux >= 50 && lux <= 1000) {
+    document.body.className = 'normal';
+  }
+  if(lux > 1000)  {
+    document.body.className = 'bright';
+  }
+  
+});
+
+window.addEventListener('lightlevel', function(e) {
+  var level = e.value; document.querySelector('#resultLevel').textContent = level;
+  
+  if(level === 'dim') {
+    document.body.className = 'dim';
+  }
+  if(level === 'normal')  {
+    document.body.className = 'normal';
+  }
+  if(level === 'bright')  {
+    document.body.className = 'bright';
+  }
 });
